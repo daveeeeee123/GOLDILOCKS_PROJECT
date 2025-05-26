60deb8ea2c42b670ad68f90e1b2b74872551849f
daveeeeee123 <dcadilo@yahoo.com>
2025-05-27 04:10:44 +0800

    Form-template

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
     <link rel="style.csss">
 <body>
 
-
+<form action="output.php" method="post">
+   <label for="BranchID">Branch ID</label>
+        <input type="text"  name="BranchID"></input><br>
+   <label for="Location">Location</label>
+        <input type="text" name="Location"></input><br>
+   <label for="BranchID">Branch ID</label>
+        <input type="text" name="Owner"></input><br>  
+   <label for="CompanyID">Company ID</label>
+    <input type="number" name="CompanyID"></input><br>
+    <input type="submit" name="submit" value="submit"></input>
+    </form>
 </body>
     
 
---
+++ output.php
@@ -0,0 +1,8 @@
+<?php 
+
+if(isset($_POST['submit'])){
+  echo "Send";
+}
+else{
+    echo "Di na send";
+}
